1183aa781e36dc5c491cbbad03763d1aaf9f23f9
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2024-09-18 01:39:08 -0700

    Added init file

---
+++ postboy.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+
+# Get all possible arguments
+# -u: Base URL
+# -m: Request method
+# -e: endpoint
+# -b: Bearer Token
+# -d: Request Body
+# -h..k: Headers
+
+# Set Strings to build
+CURL_CMD="curl --silent --location "
+FULL_URL=""
+REQUEST_BODY=""
+HEADERS=""
+
+# Save data from last request here
+declare -A request_details
+
+while getopts "u:m:e:b:d:h:i:j:k:" arg
+do
+
+    case "${arg}" in
+        u)
+          # Base URL
+          echo "URL: $OPTARG"
+          FULL_URL="${OPTARG}"
+          request_details["URL"]="${OPTARG}"
+          ;;
+        
+        m)
+          # Method 
+          echo "Method: $OPTARG"
+          CURL_CMD="${CURL_CMD} --request ${OPTARG}"
+          request_details["METHOD"]="${OPTARG^^}"
+          ;;
+        e)
+          # Endpoint
+          echo "Endpoint: $OPTARG"
+          FULL_URL="${FULL_URL}${OPTARG}"
+          request_details["ENDPOINT"]="${OPTARG}"
+          ;;
+        b)
+          # Bearer Token (JWT)  
+          echo "Token: $OPTARG"
+          HEADERS="${HEADERS} --header 'Authorization: Bearer ${OPTARG}'"
+          request_details["TOKEN"]="${OPTARG}"
+          ;;
+        d)
+          # Request Body
+          echo "Request Body: $OPTARG"
+          REQUEST_BODY="--data '${OPTARG}' --header 'Content-Type: application/json'"
+          request_details["REQUEST_BODY"]="${OPTARG}"
+          ;;
+        h)
+          # Headers
+          echo "Header: $OPTARG"
+          HEADERS="${HEADERS} --header '${OPTARG}'"
+          request_details["HEADERS"]="${request_details["HEADERS"]} ${HEADERS}"
+          ;;
+        i)
+          # Headers
+          echo "Header: $OPTARG"
+          HEADERS="${HEADERS}${OPTARG}"
+          request_details["HEADERS"]="${request_details["HEADERS"]} ${HEADERS}"
+          ;;
+        j)
+          # Headers
+          echo "Header: $OPTARG"
+          HEADERS="${HEADERS}${OPTARG}"
+          request_details["HEADERS"]="${request_details["HEADERS"]} ${HEADERS}"
+          ;;
+        k)
+          # Headers
+          echo "Header: $OPTARG"
+          HEADERS="${HEADERS}${OPTARG}"
+          request_details["HEADERS"]="${request_details["HEADERS"]} ${HEADERS}"
+          ;;
+       \?)
+          echo "[DEFAULT CASE] var: $var"
+          ;;
+
+    esac
+done
+
+# Proces request if any scripts args are passed in
+if [[ ! -z "${request_details[URL]}" ]]; then
+
+    # eval "${CURL_CMD} '${FULL_URL}' ${HEADERS} ${REQUEST_BODY} | jq"
+    new_result=$(eval "${CURL_CMD} '${request_details[URL]}${request_details[ENDPOINT]}' ${request_details[HEADERS]} ${request_details[REQUEST_BODY]}")
+
+    contains_error=$(jq -r '.' <<< "${new_result}" 2>&1 | grep 'parse error' | wc -l)
+
+    if [[ ${contains_error} -gt 0 ]]; then
+        echo "${new_result}"
+    else
+        echo ${new_result} | jq
+    fi
+fi
+
+
+
+
+
+while true
+do
+
+  # Read info for new request
+  echo -e "\n\n======== New request ========\n\nURL: ${request_details[URL]}\nHeaders: ${request_details[HEADERS]}\nEndpoint: ${request_details[ENDPOINT]}\n"
+  
+  # Make sure we require a URL
+  if [[ -z "${request_details[URL]}" ]]; then
+      read -p "URL: " new_url
+
+      [[ -z "${new_url}" ]] && echo "A URL is required!" && continue
+  fi
+ 
+  endpoint_note=""
+  [[ ! -z "${request_details[ENDPOINT]}" ]] && endpoint_note=" (leave blank to use previous)"
+  read -p "Endpoint:$endpoint_note " new_endpoint
+  read -p "HTTP Method: " new_method
+  read -p "Request Body: " new_body
+ 
+  [[ ! -z "${new_endpoint}" ]] && request_details["ENDPOINT"]=${new_endpoint}
+  [[ ! -z "${new_method}" ]] && request_details["METHOD"]=${new_method^^}
+  [[ ! -z "${new_body}" ]] && request_details["REQUEST_BODY"]="--data '${new_body}' --header 'Content-Type: application/json'"
+  [[ ! -z "${new_url}" ]] && request_details["URL"]=${new_url}
+  
+  new_result=$(eval "${CURL_CMD} '${request_details[URL]}${request_details[ENDPOINT]}' ${request_details[HEADERS]} ${request_details[REQUEST_BODY]}")
+ 
+  contains_error=$(jq -r '.' <<< "${new_result}" 2>&1 | grep 'parse error' | wc -l)
+  
+  if [[ ${contains_error} -gt 0 ]]; then
+      echo "${new_result}"
+  else
+      echo ${new_result} | jq
+  fi
+
+done
+
+
+
+#eval "${CMD} | jq"
+#echo $CMD
+#RESPONSE=$($CMD)
+#echo $RESPONSE
+#curl --silent --location --request POST 'https://api.dropboxapi.com/2/users/get_current_account' \
+#--header 'Dropbox-API-Select-User: dbmid:AADLAjqwKs8-CVxgznCcJrcEEK6XY2Q0d0U' \
+#--header 'Authorization: Bearer SHtBgI-VCogAAAAAAAAAAbN30p4IEgwXbEXVczdpGEJEDKSB0G_LfBZX1d1e80DC' | jq
+
+#bash postboy.sh  -m POST -u 'https://api.dropboxapi.com' -e '/2/users/get_current_account'  -b 'SHtBgI-VCogAAAAAAAAAAbN30p4IEgwXbEXVczdpGEJEDKSB0G_LfBZX1d1e80DC' -h 'Dropbox-API-Select-User: dbmid:AADLAjqwKs8-CVxgznCcJrcEEK6XY2Q0d0U'
+
